a895deba90818b7500fbaac98b7d34f4eda8793f
Daniel Belz <Daniel.Belz@EXXETA.com>
2019-09-18 09:48:16 +0200

    Add project files.

---
+++ samples/Paipurain.Samples.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Paipurain.Samples.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
