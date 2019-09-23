e5700db89cf184d2a8b494f74ff5de2ff4670a82
Daniel Belz <daniel_belz@gmx.net>
2019-09-23 18:50:16 +0200

    Splitted the pipeline builder into different source files

--- src/Paipurain/PipelineBuilder.cs
+++ src/Paipurain/PipelineBuilder.cs
@@ -1,12 +1,9 @@
-﻿using Paipurain.Application;
-
-using System;
-using System.Threading.Tasks;
+﻿using System;
 using System.Threading.Tasks.Dataflow;
 
 namespace Paipurain
 {
-    public class PipelineBuilder<TInput, TOutput>
+    public partial class PipelineBuilder<TInput, TOutput>
     {
         private IDataflowBlock _initialUnit;
         private IDataflowBlock _lastUnit;
@@ -23,56 +20,6 @@ namespace Paipurain
             LinkToPredecessorBlock(block);
 
             return this;
-        }
-
-        public PipelineBuilder<TInput, TOutput> AddUnit<TTransformFunctionInput, TTransformFunctionOutput>(
-            Func<TTransformFunctionInput, Task<TTransformFunctionOutput>> asyncTransformFunction)
-        {
-            if (asyncTransformFunction == null)
-                throw new ArgumentNullException();
-
-            var block = new TransformBlock<TransformWrapper<TOutput>, TransformWrapper<TOutput>>(
-                async (unit) => new TransformWrapper<TOutput>(await asyncTransformFunction(unit.Value), unit.Completion));
-
-            LinkToPredecessorBlock(block);
-
-            return this;
-        }
-
-        public IPipeline<TInput, TOutput> Build()
-        {
-            var initialUnit = _initialUnit as ITargetBlock<TransformWrapper<TOutput>>;
-
-            var resultUnit = new ActionBlock<TransformWrapper<TOutput>>((tc) =>
-                tc.Completion.SetResult((TOutput)tc.Value));
-
-            if (!(_lastUnit is ISourceBlock<TransformWrapper<TOutput>> tailSourceUnit))
-                throw new InvalidOperationException();
-
-            tailSourceUnit.LinkTo(resultUnit);
-
-            return new Pipeline<TInput, TOutput>(initialUnit);
-        }
-
-        private void LinkToPredecessorBlock(IDataflowBlock block)
-        {
-            if (block == null)
-                return;
-
-            if (_initialUnit == null)
-                _initialUnit = block;
-
-            if (_lastUnit == null)
-            {
-                _lastUnit = block;
-                return;
-            }
-
-            if (!(_lastUnit is ISourceBlock<TransformWrapper<TOutput>> sourceBlock))
-                return;
-
-            sourceBlock.LinkTo(block as ITargetBlock<TransformWrapper<TOutput>>);
-            _lastUnit = block;
         }
     }
 }
