df434140d1cb0dab471803d4d50863414a2c337c
Daniel Belz <daniel_belz@gmx.net>
2019-09-24 00:02:50 +0200

    Updated sample project

--- samples/Paipurain.Samples.ConsoleApp/Program.cs
+++ samples/Paipurain.Samples.ConsoleApp/Program.cs
@@ -1,28 +1,29 @@
 ﻿using Paipurain.Builder;
 
 using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Paipurain.Samples.ConsoleApp
 {
     class Program
     {
-        static async Task Main()
+        static async Task Main(string[] args)
         {
-            var pipeline = new PipelineBuilder<bool>()
-                .AddBlock<bool, string>(BeginMethod)
-                .AddBlock<string, bool>(async (t) => await Task.FromResult(true))
+            var pipeline = new PipelineBuilder<string[], string>()
+                .AddBlock<string[], string>(GetFilePath)
+                .AddBlock<string, string>(ReadFileContent)
+                .AddBlock<string, string>(AttachText)
                 .Build();
 
-            var value = await pipeline.Process(true);
+            var value = await pipeline.Process(args);
 
             Console.WriteLine(value);
             Console.ReadLine();
         }
 
-        private static string BeginMethod(bool input)
-        {
-            return input ? ":)" : ":(";
-        }
+        private static string GetFilePath(string[] args) => args[0];
+        private static string ReadFileContent(string path) => File.ReadAllText(path);
+        private static string AttachText(string content) => $"{content} from the command line parsing sample!";
     }
 }
