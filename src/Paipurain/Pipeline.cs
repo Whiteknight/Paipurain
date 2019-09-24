cd0385f65ec412f2d8c076ad7eeab6de918c6214
Daniel Belz <Daniel.Belz@EXXETA.com>
2019-09-24 11:40:21 +0200

    Changed license and renamed some fields properties etc.

--- src/Paipurain/Pipeline.cs
+++ src/Paipurain/Pipeline.cs
@@ -6,18 +6,18 @@ namespace Paipurain
 {
     internal class Pipeline<TInput, TOutput> : IPipeline<TInput, TOutput>
     {
-        private readonly ITargetBlock<TransformWrapper<TOutput>> _beginBlock;
+        private readonly ITargetBlock<TransformWrapper<TOutput>> _initialBlock;
         private readonly TaskCompletionSource<TOutput> _completion;
 
-        internal Pipeline(ITargetBlock<TransformWrapper<TOutput>> beginBlock)
+        internal Pipeline(ITargetBlock<TransformWrapper<TOutput>> initialBlock)
         {
-            _beginBlock = beginBlock ?? throw new ArgumentNullException();
+            _initialBlock = initialBlock ?? throw new ArgumentNullException();
             _completion = new TaskCompletionSource<TOutput>();
         }
 
         public Task<TOutput> Process(TInput input)
         {
-            _beginBlock.SendAsync(new TransformWrapper<TOutput>(input, _completion));
+            _initialBlock.SendAsync(new TransformWrapper<TOutput>(input, _completion));
 
             return _completion.Task;
         }
